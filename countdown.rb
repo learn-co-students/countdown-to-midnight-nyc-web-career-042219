b2759afd170006de23e7f4fd6019763fa7a8e253
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-04 20:54:07 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(int)
+number = 10
+
+  while number > 0
+    
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    end
+      
+end
+
+def countdown_with_sleep
+  sleep(5) 
 end
